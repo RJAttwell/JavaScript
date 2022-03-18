75a520f0cc85bb6c79130d7b25c84b0c01dca0e8
RJAttwell <richardjattwell@gmail.com>
2022-03-18 23:52:54 +0000

    Add files via upload

---
+++ Basic Arithmetic.js
@@ -0,0 +1,24 @@
+var cost = 3 + (5 * 2); //Good practice to add brackets even though you know the multiplication will happen first
+
+//Dog to Human age converter 
+var dogAge = prompt("What is your dog's age?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog would be " + humanAge + " years old in human years");
+
+//Increment 
+
+var x = 5;
+x++; //The same as writing x + 1 
+x--; //The same as writing x - 1
+x+=2; //X = x+2
+
+var x = 5
+var y = 3
+x += y; //Allows you to increment with another variable 
+
+//Modulo 
+
+//Gives remainder 
+
+var a = 16 % 4;
+//Would equal 0 
