eb36ae1a51f108275e63af3792d14b22e97be69f
RJAttwell <richardjattwell@gmail.com>
2022-04-08 13:12:36 +0100

    Add files via upload

---
+++ Fibonacci Code Challenge.js
@@ -0,0 +1,25 @@
+//Fibonacci Code Challenge
+
+
+function fibonacciCode(n){
+
+    var output = []; //Create Array
+
+    if(n === 1){    //If statement to output 0 in the array if n is equal to 1
+        output = [0];
+    }
+    else if(n === 2){  //If-Else statement to output 0, 1 in the array if n is equal to 2 
+        output = [0, 1];
+    }
+    else{
+        output = [0, 1];
+        for(var i = 2; i < n; i++){
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    
+    return output;
+}
+
+output = fibonacciCode();
+console.log(output);
