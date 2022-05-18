4ddafb306033d80a5c2370280d9b66feb099530c
RJAttwell <richardjattwell@gmail.com>
2022-05-18 13:12:06 +0100

    Add files via upload

--- JavaScript Objects.js
+++ JavaScript Objects.js
@@ -54,3 +54,45 @@ var bellBoy1 = {
 
 bellBoy1.moveSuitcase();
 //Difference between method and property is the ()
+
+//Same with a constructor function 
+function BellBoy(name, age, workPermit, languages){
+    this.name = name;
+    this.age = age;
+    this.workPermit = workPermit;
+    this.languages = languages;
+    this.moveSuitcase = function(){
+        alert("May I take your suitcase?")
+        pickUpSuitcase();
+        move();
+    }
+}
+
+//Another example
+function HouseKeeper(name, yearsExp, nameOfPrevHotels, age){
+    this.name = name;
+    this.yearsExp = yearsExp;
+    this.nameOfPrevHotels = nameOfPrevHotels;
+    this.age = age;
+	this.clean = function() {
+		alert("Cleaning in progress...")
+	}
+}
+
+//To call: 
+houseKeeper1.clean();
+
+//Can see it is similar to code used in drum kit code:
+var crash = new Audio("sounds/crash.mp3");
+    crash.play(); 
+
+//Cannot see the source code but probably looks like:
+function Audio(fileLocation){
+    this.fileLocation = fileLocation;
+    this.play = function(){
+        //Tap into audio hardware
+        //Check file at fileLocation exists
+        //Check file at fileLocation is a sound file
+        //Play file at fileLocation
+    }
+}
